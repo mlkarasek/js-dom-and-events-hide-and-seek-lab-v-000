0bc21b48b6fe872388e5fc0dc79e04f08cd8a4d5
Monica  Karasek <mlkarasek@gmail.com>
2018-09-20 20:29:51 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  
+}
+
+function deepestChild(){
+  
+}
