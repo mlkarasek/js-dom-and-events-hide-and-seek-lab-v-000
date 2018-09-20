2777681a317a1bf2422936a35e0eb89376f08e06
Monica  Karasek <mlkarasek@gmail.com>
2018-09-20 20:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,25 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list')
 
-}
+  for for (let i = 0, l = lists.length; i < l; i++) {
+     let children = lists[i].children
+
+     for (let j = 0, k = children.length; j < k; j++) {
+       children[j].innerHTML = parseInt(children[j].innerHTML) + n
+     }
+   }
+ }
+
 
 function deepestChild(){
+  let node = document.getElementById('grand-node')
+    let nextNode = node.children[0]
 
+    while (nextNode) {
+      node = nextNode
+      nextNode = node.children[0]
+}
+return node
 }
